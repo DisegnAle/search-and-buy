dd1ee90b725a685d200bf55d8eecd1b1378a6de6
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-20 10:04:55 +0100

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnYQuIVfMXZW_-nVkO7DS7OjRQ4WO2Kts",
+  authDomain: "search-and-buy.firebaseapp.com",
+  databaseURL: "https://search-and-buy-default-rtdb.firebaseio.com",
+  projectId: "search-and-buy",
+  storageBucket: "search-and-buy.appspot.com",
+  messagingSenderId: "648869467094",
+  appId: "1:648869467094:web:84de59c909add3e3daca77",
+};
+firebase.initializeApp(firebaseConfig);
+
+// utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// collection references
+const productsCollection = db.collection("products");
+
+// export utils/refs
+export { db, auth, productsCollection };
