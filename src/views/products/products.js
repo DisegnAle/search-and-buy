bc906862941b8c6d3bb49c36da5355b76e2bd314
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-20 11:04:01 +0100

    products layout

--- src/views/products/products.js
+++ src/views/products/products.js
@@ -1,6 +1,11 @@
+import productsList from "@/components/products-list/index.vue";
+import searchForm from "@/components/search-form/index.vue";
 export default {
   name: "products",
-  components: {},
+  components: {
+    productsList,
+    searchForm,
+  },
   props: [],
   data() {
     return {};
