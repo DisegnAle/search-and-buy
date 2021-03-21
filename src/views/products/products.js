40cca699b8bff55182b71a6e66d2f6b91e8dbde6
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-21 10:10:07 +0100

    basket in layout

--- src/views/products/products.js
+++ src/views/products/products.js
@@ -1,10 +1,12 @@
 import searchResults from "@/components/search-results/index.vue";
 import searchForm from "@/components/search-form/index.vue";
+import basket from "@/components/basket/index.vue";
 export default {
   name: "products",
   components: {
     searchResults,
     searchForm,
+    basket,
   },
   props: [],
   data() {
